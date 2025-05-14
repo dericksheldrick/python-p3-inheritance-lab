3e2722664777fefbba50ad1f2f4fa31b8fc511a0
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-05-14 07:56:10 +0300

    Assignment submission

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+class teacher(User):
+    def __init__(self, first_name, last_name):
+        super.__init__(self, first_name, last_name)
+            
+class student(User):
+    def __init__(self, first_name, last_name):
+        super.__init__(self, first_name, last_name)
